ac91ba35b3c7113644ea25cf84156a3aad3a031a
Vanderson de Oliveira Souza <vandersonsouzati@gmail.com>
2020-11-22 12:36:32 -0300

    inicio projeto

---
+++ cursoSpringBootLivro/src/main/java/com/example/cursoSpringBootLivro/CursoSpringBootLivroApplication.java
@@ -0,0 +1,13 @@
+package com.example.cursoSpringBootLivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootLivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootLivroApplication.class, args);
+	}
+
+}
---
+++ cursoSpringBootLivro/src/test/java/com/example/cursoSpringBootLivro/CursoSpringBootLivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cursoSpringBootLivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootLivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
